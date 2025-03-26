932b2f8d7bd7f53a77a6c787a67cc8c1a67f1577
Backend <dqthang0705@gmail.com>
2025-03-26 22:26:41 +0700

    add bird1 animation

--- src/app/Game/scenes/BackgroundScene.tsx
+++ src/app/Game/scenes/BackgroundScene.tsx
@@ -1,25 +1,60 @@
 "use client";
 
 export default class BackgroundScene extends Phaser.Scene {
-  private bg!: Phaser.GameObjects.Image;
+  count: number;
+  rope: any;
+  private bg1!: Phaser.GameObjects.TileSprite;
+  private bg2!: Phaser.GameObjects.TileSprite;
+  private bird1!: Phaser.GameObjects.Sprite;
   constructor() {
     super("BackgroundScene");
+    this.count = 0;
   }
   create() {
-    // const sizeWidth = window.innerWidth
-    console.log(this.scale.width);
-    console.log(this.scale.height);
+    this.bg1 = this.add.tileSprite(
+      this.scale.width / 2,
+      this.scale.height / 2,
+      this.scale.width,
+      this.scale.height,
+      "background"
+    );
+    this.bg2 = this.add
+      .tileSprite(
+        900,
+        this.scale.height / 2,
+        this.scale.width,
+        this.scale.height,
+        "background"
+      )
+      .setFlipX(true);
 
-    this.bg = this.add
-      .image(this.scale.width, this.scale.height, "background")
-      .setOrigin(1, 1);
-    //   .setDisplayOrigin(this.scale.width, this.scale.height);
-
-    this.updateBackgroundSize();
-    this.scale.on("resize", this.updateBackgroundSize, this);
+    this.bird1 = this.physics.add.sprite(300, 200, "bird_spr");
+    this.anims.create({
+      key: "flappy",
+      frames: this.anims.generateFrameNumbers("bird_spr", { start: 0, end: 3 }),
+      frameRate: 10,
+      repeat: -1,
+    });
+    this.bird1.setScale(0.5);
+    this.bird1.play("flappy");
   }
+  update() {
+    let speed = 1.3;
 
+    // Di chuyển cả hai ảnh nền
+    this.bg1.x -= speed;
+    this.bg2.x -= speed;
+
+    // Khi ảnh đầu tiên ra khỏi màn hình, đưa nó về vị trí phía sau ảnh thứ hai
+    if (this.bg1.x + this.bg1.width <= 300) {
+      this.bg1.x = this.bg2.x + this.bg2.width - 2;
+    }
+    if (this.bg2.x + this.bg2.width <= 300) {
+      this.bg2.x = this.bg1.x + this.bg1.width;
+    }
+  }
   updateBackgroundSize() {
-    this.bg.setDisplaySize(this.scale.width, this.scale.height);
+    this.bg1.setDisplaySize(this.scale.width, this.scale.height);
+    this.bg2.setDisplaySize(this.scale.width, this.scale.height);
   }
 }
--- src/app/Game/scenes/PreLoadScene.tsx
+++ src/app/Game/scenes/PreLoadScene.tsx
@@ -1,11 +1,19 @@
 "use client";
+
+import { assert } from "console";
+
 export default class PreLoadScene extends Phaser.Scene {
   constructor() {
     super("PreLoadScene");
   }
 
-  preload() {
-    this.load.image("background", "asset/Background1.png");
+  preload(this: Phaser.Scene) {
+    this.load.setPath("asset");
+    this.load.image("background", "Background1.png");
+    this.load.spritesheet("bird_spr", "bird1_spritesheet.png", {
+      frameWidth: 300,
+      frameHeight: 216,
+    });
   }
 
   create() {
