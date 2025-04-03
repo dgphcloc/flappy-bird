422b853ac77f531c073137ba993e9955e03c34ee
Backend <dqthang0705@gmail.com>
2025-04-03 09:16:16 +0700

    add asset Icon FB

--- src/app/Game/scenes/PreLoadScene.tsx
+++ src/app/Game/scenes/PreLoadScene.tsx
@@ -1,39 +1,66 @@
 "use client";
 
-import { assert } from "console";
-
 export default class PreLoadScene extends Phaser.Scene {
   constructor() {
     super("PreLoadScene");
   }
 
-  preload(this: Phaser.Scene) {
-    this.load.setPath("asset");
-    this.load.image("background", "Background1.png");
-    this.load.spritesheet("bird1_spr", "bird1_spritesheet.png", {
-      frameWidth: 300,
-      frameHeight: 216,
+  preload() {
+    // Load background images
+    this.load.image("background", "assets/images/background.png");
+    this.load.image("background_frame", "assets/images/background_frame.png");
+
+    // Load UI elements
+    this.load.image("username_icon", "assets/images/username_icon.png");
+    this.load.image("password_icon", "assets/images/password_icon.png");
+    this.load.image("eye_icon", "assets/images/eye_icon.png");
+    this.load.image("spr_btn_x", "assets/images/spr_btn_x.png");
+
+    // Load social media icons
+    this.load.spritesheet("icon_GG", "assets/images/icon_GG.png", {
+      frameWidth: 32,
+      frameHeight: 32,
     });
-    this.load.image("TextTitle", "TextTitle.png");
-    this.load.spritesheet("button_menu", "spritesheet button menu.png", {
-      frameWidth: 310,
-      frameHeight: 100,
+    this.load.spritesheet("icon_FB", "assets/images/icon_FB.png", {
+      frameWidth: 32,
+      frameHeight: 32,
     });
-    this.load.image("background_frame", "background_frame.png");
-    this.load.spritesheet("spr_btn_x", "spritesheet_btn_x.png", {
-      frameWidth: 78,
-      frameHeight: 78,
+
+    // Add loading bar
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
+
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    const loadingText = this.add.text(
+      width / 2,
+      height / 2 - 50,
+      "Loading...",
+      {
+        font: "20px monospace",
+        color: "#ffffff",
+      }
+    );
+    loadingText.setOrigin(0.5, 0.5);
+
+    this.load.on("progress", (value: number) => {
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
     });
-    this.load.image("username_icon", "iconUser.png");
-    this.load.image("password_icon", "iconPassword.png");
-    this.load.image("eye_icon", "eye_icon.png");
-    this.load.spritesheet("icon_GG", "icon_GG.png", {
-      frameWidth: 53,
-      frameHeight: 53,
+
+    this.load.on("complete", () => {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      this.scene.start("LoginScene");
     });
   }
 
   create() {
-    this.scene.start("MenuLoginScene");
+    // Scene will automatically transition to LoginScene when loading is complete
   }
 }
