4a33237bd787ee7009eeae99031da2c998c48daa
Backend <dqthang0705@gmail.com>
2025-04-09 20:01:56 +0700

    fix Origin ground

--- src/app/Game/scenes/BackgroundScene.tsx
+++ src/app/Game/scenes/BackgroundScene.tsx
@@ -4,39 +4,54 @@ export default class BackgroundScene extends Phaser.Scene {
   rope: any;
   private bg1!: Phaser.GameObjects.TileSprite;
   private bg2!: Phaser.GameObjects.TileSprite;
+  private ground1!: Phaser.GameObjects.TileSprite;
+  private ground2!: Phaser.GameObjects.TileSprite;
   constructor() {
     super("BackgroundScene");
   }
   create() {
+    const widthBG = 1080;
+    const heightBG = 1920;
+
     this.bg1 = this.add
-      .tileSprite(0, 0, 1080, 1920, "background")
+      .tileSprite(0, 0, widthBG, heightBG, "background")
       .setOrigin(0, 0); // Đặt từ góc trái
 
     this.bg2 = this.add
-      .tileSprite(this.scale.width, 0, 1080, 1920, "background")
+      .tileSprite(this.scale.width, 0, widthBG, heightBG, "background")
       .setOrigin(0, 0)
       .setFlipX(true);
 
-    // const sizeWidth = this.scale.width;
-    // const sizeHeight = this.scale.height;
-    // const updatebg = (width: number, height: number) => {
-    //   const scalex = width / this.bg1.width;
-    //   const scaley = height / this.bg1.height;
-    //   const scalebg1 = Math.min(scalex, scaley);
-    //   this.bg1.setScale(scalebg1);
-    //   this.bg2.setScale(scalebg1);
-    // };
+    const widthGround = 1536;
+    const heightGround = 430;
+
+    this.ground1 = this.add
+      .tileSprite(0, 0, widthGround, heightGround, "ground")
+      .setOrigin(0.5, 1);
+    this.ground2 = this.add
+      .tileSprite(0, 0, widthGround, heightGround, "ground")
+      .setOrigin(0.5, 1);
+
+    const groundScaleX = this.scale.width / widthGround;
+    const groundScaleY = this.scale.height / heightGround;
+
+    const groundScale = Math.min(groundScaleX, groundScaleY) * 1.3; // Nhân với 0.4 để giữ tỷ lệ với background
+    this.ground1.setScale(groundScale);
+    this.ground1.setPosition(this.scale.width / 2, this.scale.height);
+
+    this.ground2.setScale(groundScale);
+    this.ground2
+      .setPosition(
+        this.ground1.x + this.ground1.displayWidth,
+        this.scale.height
+      )
+      .setFlipX(true);
 
     const scaleX = this.scale.width / 1080;
     const scaleY = this.scale.height / 1920;
     const scale = Math.min(scaleX, scaleY); // Chọn scale lớn nhất để ảnh luôn full màn hình
-
-    this.bg1.setScale(scale); // Scale ảnh
-    const scaleX1 = this.scale.width / 1080;
-    const scaleY1 = this.scale.height / 1920;
-    const scale1 = Math.min(scaleX1, scaleY1); // Chọn scale lớn nhất để ảnh luôn full màn hình
-
-    this.bg2.setScale(scale1); // Scale ảnh
+    this.bg1.setScale(scale);
+    this.bg2.setScale(scale); //
     // this.bg1.setPosition(this.scale.width / 2, this.scale.height / 2); // Căn giữa
 
     // updatebg(sizeWidth, sizeHeight);
@@ -54,8 +69,10 @@ export default class BackgroundScene extends Phaser.Scene {
     // });
   }
   update() {
-    this.bg1.x -= 2;
-    this.bg2.x -= 2;
+    this.bg1.x -= 1;
+    this.bg2.x -= 1;
+    this.ground1.x -= 2;
+    this.ground2.x -= 2;
 
     // Nếu `bg1` ra khỏi màn hình, chuyển nó sang phải nối tiếp `bg2`
     if (this.bg1.x <= -this.scale.width) {
@@ -65,6 +82,14 @@ export default class BackgroundScene extends Phaser.Scene {
     // Nếu `bg2` ra khỏi màn hình, chuyển nó sang phải nối tiếp `bg1`
     if (this.bg2.x <= -this.scale.width) {
       this.bg2.x = this.bg1.x + this.scale.width;
+    }
+
+    // Sửa lại điều kiện cho ground, tính toán dựa trên origin ở giữa
+    if (this.ground1.x <= -this.ground1.displayWidth / 2) {
+      this.ground1.x = this.ground2.x + this.ground2.displayWidth;
+    }
+    if (this.ground2.x <= -this.ground2.displayWidth / 2) {
+      this.ground2.x = this.ground1.x + this.ground1.displayWidth;
     }
   }
 }
