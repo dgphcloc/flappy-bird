89a4ff44ed84ff6497e8391c340121b72037c245
@dgphcloc <dgphcloc@gmail.com>
2025-04-12 18:24:55 +0700

    feat(auth): enhance useCurrentUser to allow picking user attributes

--- src/hooks/useCurrentUser.tsx
+++ src/hooks/useCurrentUser.tsx
@@ -1,8 +1,15 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useMemo } from "react";
 import type { User } from "@supabase/supabase-js";
 import useSupabaseClient from "@/lib/supabase/client";
-
-function useCurrentUser() {
+/**
+ * const user = useCurrentUser(['id', 'email']);
+ * user has type  { id: string; email: string } | null
+ */
+function useCurrentUser(): User | null;
+function useCurrentUser<T extends keyof User>(
+  select: T[]
+): Pick<User, T> | null;
+function useCurrentUser(select?: Array<keyof User>) {
   const supabase = useSupabaseClient();
   const [user, setUser] = useState<User | null>(null);
 
@@ -22,13 +29,29 @@ function useCurrentUser() {
         setUser(null);
       }
     });
+
     return () => {
       isMounted = false;
       subscription?.unsubscribe();
     };
   }, [supabase]);
 
-  return user;
+  const selectedUser = useMemo(() => {
+    if (!user) return null;
+    if (select) {
+      return pick(user, select);
+    }
+    return user;
+  }, [user, select]);
+
+  return selectedUser;
+}
+
+function pick<T, K extends keyof T>(obj: T, keys: K[]): Pick<T, K> {
+  return keys.reduce((acc, key) => {
+    acc[key] = obj[key];
+    return acc;
+  }, {} as Pick<T, K>);
 }
 
 export default useCurrentUser;
