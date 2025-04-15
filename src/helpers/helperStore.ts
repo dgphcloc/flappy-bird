3366171b2ab078995a30f5c7f8fc6c5937a91bc0
@dgphcloc <dgphcloc@gmail.com>
2025-04-15 16:02:08 +0700

    add isValidScore to check score is valid

--- src/helpers/helperStore.ts
+++ src/helpers/helperStore.ts
@@ -1,7 +1,20 @@
-const generateUserEmailTemp = (): string => {
+import getUserSession from "@/lib/supabase/getUserSession";
+
+export const generateUserEmailTemp = (): string => {
   const timestamp = Date.now();
   const email = `$user_${timestamp}@flappybird.com`;
   return email;
 };
 
-export { generateUserEmailTemp };
+export const isLogged = async () => !!(await getUserSession()).data.session;
+
+export const isValidScore = (score: number | string): boolean => {
+  const MAX_SCORE = 2_000_000_000;
+  const MIN_SCORE = 0;
+
+  const numericScore = typeof score === "string" ? parseFloat(score) : score;
+
+  if (isNaN(numericScore)) return false;
+
+  return numericScore >= MIN_SCORE && numericScore <= MAX_SCORE;
+};
