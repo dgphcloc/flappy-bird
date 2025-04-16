2bf5e599fa4b8b7c44bd1c4fd383cc2d0685b3c3
@dgphcloc <dgphcloc@gmail.com>
2025-04-16 15:26:32 +0700

    add profile component to show user info

---
+++ src/components/profile.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import useCurrentUser from "@/hooks/useCurrentUser";
+
+export default function Profile() {
+  const user = useCurrentUser(["id", "email"]);
+
+  return (
+    <div>
+      <h1>Thông tin người dùng</h1>
+      {user ? <pre>{user.email}</pre> : <p>Chưa đăng nhập</p>}
+    </div>
+  );
+}
