2fdb98375deec6ae58d4f1d6a1d155b0b7736dcc
@dgphcloc <dgphcloc@gmail.com>
2025-04-16 14:59:37 +0700

    make table responsive

--- src/components/table/table.tsx
+++ src/components/table/table.tsx
@@ -21,49 +21,54 @@ export default function TableMantine({ data }: TableMantineProps) {
         boxShadow: "0 1px 3px rgba(0,0,0,0.1)",
       }}
     >
-      <Table striped highlightOnHover>
-        <Table.Thead>
-          <Table.Tr>
-            {data.head.map((header, index) => (
-              <Table.Th key={index}>{header}</Table.Th>
-            ))}
-          </Table.Tr>
-        </Table.Thead>
-        <Table.Tbody>
-          {data.body.map((row, rowIndex) => (
-            <Table.Tr
-              key={rowIndex}
-              style={{ cursor: "pointer" }}
-              onClick={() => {
-                const userId = row.at(0);
-                if (typeof userId === "string" || typeof userId === "number") {
-                  router.push(`/admin/users/${userId}`);
-                }
-              }}
-            >
-              {row.map((cell, cellIndex) => {
-                const isImage =
-                  typeof cell === "string" &&
-                  (cell.startsWith("http://") || cell.startsWith("https://"));
-
-                return (
-                  <Table.Td key={cellIndex}>
-                    {isImage ? (
-                      <Avatar
-                        src={cell}
-                        alt="image"
-                        style={{ border: "2px solid #4C4C4C" }}
-                      />
-                    ) : (
-                      cell
-                    )}
-                  </Table.Td>
-                );
-              })}
+      <Table.ScrollContainer minWidth={500}>
+        <Table striped highlightOnHover>
+          <Table.Thead>
+            <Table.Tr>
+              {data.head.map((header, index) => (
+                <Table.Th key={index}>{header}</Table.Th>
+              ))}
             </Table.Tr>
-          ))}
-        </Table.Tbody>
-      </Table>
+          </Table.Thead>
+          <Table.Tbody>
+            {data.body.map((row, rowIndex) => (
+              <Table.Tr
+                key={rowIndex}
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  const userId = row.at(0);
+                  if (
+                    typeof userId === "string" ||
+                    typeof userId === "number"
+                  ) {
+                    router.push(`/admin/users/${userId}`);
+                  }
+                }}
+              >
+                {row.map((cell, cellIndex) => {
+                  const isImage =
+                    typeof cell === "string" &&
+                    (cell.startsWith("http://") || cell.startsWith("https://"));
+
+                  return (
+                    <Table.Td key={cellIndex}>
+                      {isImage ? (
+                        <Avatar
+                          src={cell}
+                          alt="image"
+                          style={{ border: "2px solid #4C4C4C" }}
+                        />
+                      ) : (
+                        cell
+                      )}
+                    </Table.Td>
+                  );
+                })}
+              </Table.Tr>
+            ))}
+          </Table.Tbody>
+        </Table>
+      </Table.ScrollContainer>
     </Box>
   );
 }
