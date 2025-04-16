443d8fb73eaf99690350452d11ce8e8042aadf0a
@dgphcloc <dgphcloc@gmail.com>
2025-04-16 15:01:19 +0700

    add fetchUserById action

--- src/app/admin/adminLayout.tsx
+++ src/app/admin/adminLayout.tsx
@@ -1,15 +1,14 @@
 "use client";
-import { ReactNode } from 'react';
-import { AppShell, Burger, Group ,Box} from "@mantine/core";
+import { ReactNode } from "react";
+import { AppShell, Burger, Group, Box } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
-import Navigation from '@/components/navigation/navigation';
-
+import Navigation from "@/components/navigation/navigation";
 
 type Props = {
   children: ReactNode;
 };
-export default function AdminLayout({ children }:Props) {
-  console.log('main layout');
+export default function AdminLayout({ children }: Props) {
+  console.log("main layout");
   const [mobileOpened, { toggle: toggleMobile }] = useDisclosure();
   const [desktopOpened, { toggle: toggleDesktop }] = useDisclosure(true);
   return (
@@ -23,19 +22,26 @@ export default function AdminLayout({ children }:Props) {
     >
       <AppShell.Header style={{ backgroundColor: "#4c6ef5" }}>
         <Group h="100%" px="md">
-          <Burger opened={mobileOpened} onClick={toggleMobile} hiddenFrom="sm" size="sm" />
-          <Burger opened={desktopOpened} onClick={toggleDesktop} visibleFrom="sm" size="sm" />
+          <Burger
+            opened={mobileOpened}
+            onClick={toggleMobile}
+            hiddenFrom="sm"
+            size="sm"
+          />
+          <Burger
+            opened={desktopOpened}
+            onClick={toggleDesktop}
+            visibleFrom="sm"
+            size="sm"
+          />
         </Group>
       </AppShell.Header>
       <AppShell.Navbar style={{ backgroundColor: "#4c6ef5" }}>
         <Navigation />
       </AppShell.Navbar>
       <AppShell.Main>
-        <Box bg="#F1F3F5">
-{children}
-        </Box>
-        
-        </AppShell.Main>
+        <Box bg="#F1F3F5">{children}</Box>
+      </AppShell.Main>
     </AppShell>
   );
 }
