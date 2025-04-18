976d0b32ddee8f9c9fef9124642e675680828136
@dgphcloc <dgphcloc@gmail.com>
2025-04-18 08:51:07 +0700

    add UI to test OAuth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,38 @@
 "use client";
 import Profile from "@/components/profile";
+import useSupabaseClient from "@/lib/supabase/client";
+import { Button } from "@mantine/core";
+
 export default function AdminPage() {
-  return <Profile />;
+  const supabase = useSupabaseClient();
+
+  const loginWithGoogle = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `${location.origin}/api/auth/callback`,
+      },
+    });
+  };
+  const loginWithFacebook = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "facebook",
+      options: {
+        redirectTo: `${location.origin}/api/auth/callback`,
+      },
+    });
+  };
+  const logout = async () => {
+    await supabase.auth.signOut();
+  };
+
+  return (
+    <>
+      <Button onClick={loginWithGoogle}>google</Button>
+      <Button onClick={loginWithFacebook}>facebook</Button>
+
+      <Button onClick={logout}>log out</Button>
+      <Profile />
+    </>
+  );
 }
