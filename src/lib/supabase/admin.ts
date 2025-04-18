3d2de583c76f8b928ebe0b59770ca2c32109b81f
@dgphcloc <dgphcloc@gmail.com>
2025-04-18 10:37:12 +0700

    refactor create supabase admin instance with singleton pattern

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -1,11 +1,22 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-export default async function createSupabaseAdminAuthClient(){
-    return createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!, process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!, {
-    auth: {
+let supabaseAdminAuthClient: SupabaseClient | null = null;
+
+export default function createSupabaseAdminAuthClient() {
+  if (supabaseAdminAuthClient) {
+    return supabaseAdminAuthClient;
+  }
+
+  supabaseAdminAuthClient = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_ROLE_KEY!,
+    {
+      auth: {
         autoRefreshToken: false,
-        persistSession: false
+        persistSession: false,
+      },
     }
-    })
+  );
+
+  return supabaseAdminAuthClient;
 }
