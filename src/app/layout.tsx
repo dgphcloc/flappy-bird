fb97e0e0d743e90cb6efb80a0dd42ef745d7d601
@dgphcloc <dgphcloc@gmail.com>
2025-04-21 08:39:21 +0700

    fix forgot delete user while dlete profile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import { Providers } from "@/components/providers";
 import { MantineProvider } from "@mantine/core";
+import "@mantine/charts/styles.css";
 
 export default function RootLayout({
   children,
